a727f86fd2cdbc5b7cb7fad08965d21967c562cc
Amauri Rossetti Giovani <amaurirg@terra.com.br>
2020-06-19 18:30:10 -0300

    Included Data API

--- frontend/src/pages/main/index.js
+++ frontend/src/pages/main/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
 
+
 export default class Main extends Component {
     state = {
-        noticias: []
+        dados_imprensa: {},
+        dados_noticias: [],
     };
 
     componentDidMount() {
@@ -13,19 +15,33 @@ export default class Main extends Component {
     loadRepos = async () => {
         const url_params = "/api"
         const response = await api.get(url_params);
-        console.log(response.data);
-        this.setState({noticias: response.data});
+        // console.log(response.data);
+        const { imprensa, logo, atualizado_em, ...noticias } = response.data;
+        this.setState({
+            dados_imprensa: { imprensa, logo, atualizado_em },
+            dados_noticias: noticias.noticias
+        });
+        // console.log(imprensa);
+        // console.log(logo);
+        // console.log(atualizado_em);
+        // dados_noticias.noticias.map(noticia => (
+        //     console.log(noticia.texto)
+        // ));
+        console.log(this.state.dados_noticias);
+        // console.log(this.state.dados_noticias.noticias);
     }
 
     render() {
-        const {noticias} = this.state;
+        const { dados_imprensa, dados_noticias } = this.state;
         return (
-            <div>
-                <img src={noticias.logo} alt=""/>
+            <div className="dados_imprensa">
+                <h1>{dados_imprensa.imprensa}</h1>
+                <img src={dados_imprensa.logo} alt="Logo" />
+                <p>Atualizado em {dados_imprensa.atualizado_em}hs</p>
                 <ul>
-                {noticias.map(noticia => (
-                    console.log(noticia)
-                ))}
+                    {dados_noticias.map((noticia, contador) => (
+                        <li key={contador+1}>{noticia.texto}</li>
+                    ))}
                 </ul>
             </div>
         )
